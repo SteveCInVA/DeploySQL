c63bdb42b4502f7f4b0e1e9ba0cdd1fbc4ca97bd
Steve Carroll <stephen.carroll@microsoft.com>
2021-10-08 09:07:55 -0400

    code lint + added in sleep time resume

--- DeploySQL-Instance.ps1
+++ DeploySQL-Instance.ps1
@@ -181,7 +181,7 @@ param (
 
 $scriptVersion = '1.4.2'
 $InstallDate = Get-Date -Format "yyyy-mm-dd HH:mm:ss K"
-$StartTime = $(get-date)
+$StartTime = $(Get-Date)
 
 ##########################################
 #begin validation of parameters
@@ -748,32 +748,32 @@ Configuration InstallSQLEngine
             ValueType = "MultiString"
             ValueName = "InstallParameters"
             ValueData = @(
-                    "Computer=$Computer",
-                    "Instance=$Instance",
-                    "SQLVersion=$SQLVersion",
-                    "NumberOfNonOSDrives=$NumberOfNonOSDrives",
-                    "InstallSourcePath=$InstallSourcePath",
-                    "SQLEngineServiceAccount=$SQLEngineServiceAccount",
-                    "SQLAgentServiceAccount=$SQLAgentServiceAccount",
-                    "DBAOSAdminGroup=$DBAOSAdminGroup",
-                    "DBASQLAdminGroup=$DBASQLAdminGroup",
-                    "IsAzureVM=$IsAzureVM",
-                    "SkipDriveConfig=$SkipDriveConfig",
-                    "SkipSQLInstall=$SkipSQLInstall",
-                    "NoOpticalDrive=$NoOpticalDrive",
-                    "AddOSAdminToHostAdmin=$AddOSAdminToHostAdmin",
-                    "IsInAvailabilityGroup=$IsInAvailabilityGroup",
-                    "ClusterName=$ClusterName",
-                    "ClusterIP=$ClusterIP",
-                    "SQLAGName=$SQLAGName",
-                    "SQLHADREndpointPort=$SQLHADREndpointPort",
-                    "SQLAGIPAddr=$SQLAGIPAddr",
-                    "SQLAGPort=$SQLAGPort",
-                    "SkipSQLAGListenerCreation=$SkipSQLAGListenerCreation",
-                    "SkipSSMS=$SkipSSMS",
-                    "SkipPostDeployment=$SkipPostDeployment",
-                    "InstallCredential=$InstallUserName"
-                    )
+                "Computer=$Computer",
+                "Instance=$Instance",
+                "SQLVersion=$SQLVersion",
+                "NumberOfNonOSDrives=$NumberOfNonOSDrives",
+                "InstallSourcePath=$InstallSourcePath",
+                "SQLEngineServiceAccount=$SQLEngineServiceAccount",
+                "SQLAgentServiceAccount=$SQLAgentServiceAccount",
+                "DBAOSAdminGroup=$DBAOSAdminGroup",
+                "DBASQLAdminGroup=$DBASQLAdminGroup",
+                "IsAzureVM=$IsAzureVM",
+                "SkipDriveConfig=$SkipDriveConfig",
+                "SkipSQLInstall=$SkipSQLInstall",
+                "NoOpticalDrive=$NoOpticalDrive",
+                "AddOSAdminToHostAdmin=$AddOSAdminToHostAdmin",
+                "IsInAvailabilityGroup=$IsInAvailabilityGroup",
+                "ClusterName=$ClusterName",
+                "ClusterIP=$ClusterIP",
+                "SQLAGName=$SQLAGName",
+                "SQLHADREndpointPort=$SQLHADREndpointPort",
+                "SQLAGIPAddr=$SQLAGIPAddr",
+                "SQLAGPort=$SQLAGPort",
+                "SkipSQLAGListenerCreation=$SkipSQLAGListenerCreation",
+                "SkipSSMS=$SkipSSMS",
+                "SkipPostDeployment=$SkipPostDeployment",
+                "InstallCredential=$InstallUserName"
+            )
         }
     }
 
@@ -801,28 +801,26 @@ Configuration InstallSQLEngine
             DependsOn = '[File]InstallMediaSSMS', '[SQLSetup]Instance'
         }
 
-        Script RebootAfterSSMS
-        {
-            GetScript = {
-                $x = (Test-PendingReboot -SkipConfigurationManagerClientCheck | select-object IsRebootPending)
-                write-verbose ("IsRebootPending = " + $x.IsRebootPending)
-                return @{ Result = "!$x.IsRebootPending"}
+        Script RebootAfterSSMS {
+            GetScript  = {
+                $x = (Test-PendingReboot -SkipConfigurationManagerClientCheck | Select-Object IsRebootPending)
+                Write-Verbose ("IsRebootPending = " + $x.IsRebootPending)
+                return @{ Result = "!$x.IsRebootPending" }
             }
 
-            SetScript = {
+            SetScript  = {
                 Write-Verbose "Restarting Server"
                 Restart-Computer -Force
             }
             TestScript = {
-                $x = (Test-PendingReboot -SkipConfigurationManagerClientCheck | select-object IsRebootPending)
-                write-verbose ("IsRebootPending = " + $x.IsRebootPending)
+                $x = (Test-PendingReboot -SkipConfigurationManagerClientCheck | Select-Object IsRebootPending)
+                Write-Verbose ("IsRebootPending = " + $x.IsRebootPending)
                 !$x.IsRebootPending
             }
-            DependsOn = "[Package]SSMS"
+            DependsOn  = "[Package]SSMS"
         }
-        Log LogCompletion
-        {
-            Message = "After reboot of SSMS"
+        Log LogCompletion {
+            Message   = "After reboot of SSMS"
             DependsOn = "[Script]RebootAfterSSMS"
         }
 
@@ -839,11 +837,11 @@ Configuration ConfigureCluster
     Node $AllNodes.NodeName
     {
         PendingReboot BeforeClusterFeature {
-            Name      = "BeforeClusterFeature"
+            Name = "BeforeClusterFeature"
         }
         WindowsFeature FailoverFeature {
-            Ensure = "Present"
-            Name   = "Failover-Clustering"
+            Ensure    = "Present"
+            Name      = "Failover-Clustering"
             DependsOn = "[PendingReboot]BeforeClusterFeature"
         }
         PendingReboot AfterClusterFeature {
@@ -1109,15 +1107,15 @@ $config = @{
 }
 # configuration specific to primary node
 $config.AllNodes += @{
-    NodeName = $p
-    NodeType = 'Primary'
+    NodeName        = $p
+    NodeType        = 'Primary'
     SQLInstanceName = "$p\$Instance"
 }
 # configuration specific to all other nodes
 foreach ($c in $s) {
     $config.AllNodes += @{
-        NodeName = $c
-        NodeType = 'Secondary'
+        NodeName        = $c
+        NodeType        = 'Secondary'
         SQLInstanceName = "$c\$Instance"
     }
 }
@@ -1160,7 +1158,9 @@ if ($IsInAvailabilityGroup.IsPresent -eq $true) {
     Start-DscConfiguration -Path "$Dir\MOF\Cluster" -Wait -Verbose -CimSession $cSessions -ErrorAction Stop
 
     # visibility is lost in the above step.  pause for 5 minutes while host is rebooted, and cluster configuration is completed
-    Write-Host "##### Starting sleep cycle @ " (Get-Date)
+    $ts = New-TimeSpan -Seconds 300
+    Write-Host "##### Starting sleep cycle at " (Get-Date)
+    Write-Host "##### Script will resume at " (Get-Date) + $ts
     Start-Sleep -Seconds 300
 
     ConfigureAG -ConfigurationData $config -OutputPath "$Dir\MOF\AG"
@@ -1182,6 +1182,6 @@ if ($SkipPostDeployment.IsPresent -eq $false) {
 # remove mof files generated during install
 Remove-Item "$Dir\MOF" -Force -Recurse
 
-$elapsedTime = $(get-date) - $StartTime
+$elapsedTime = $(Get-Date) - $StartTime
 $totalTime = "{0:HH:mm:ss}" -f ([datetime]$elapsedTime.Ticks)
-write-host "Installation duration: $totalTime"
+Write-Host "Installation duration: $totalTime"
