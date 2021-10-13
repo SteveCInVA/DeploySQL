f623108d19bf74e2d937cca98b9ca7e701b70cd9
Steve Carroll <stephen.carroll@microsoft.com>
2021-10-13 13:36:47 -0400

    added working module tests

--- helperFunctions/DirectoryVerifications.psm1
+++ helperFunctions/DirectoryVerifications.psm1
@@ -81,9 +81,67 @@ function Test-ScriptIntegrity{
     Write-Verbose "Checking integrity of scripts..."
     Import-Module $InstallMediaPath\helperFunctions\Tools.psm1
 
+    $path = "$InstallMediaPath\PSModules\AccessControlDSC"
+    write-verbose "Testing $path"
+    if ('C77F157B66FA79C3FDF7977CBE7CC8754DDF48F21F204ADDD4F1B89B81C0D66F' -ne (Get-FolderHash -Path $path)){
+        Write-Warning "Script differences found in: $path"
+        $ret = $false
+    }
+
+    $path = "$InstallMediaPath\PSModules\ComputerManagementDsc"
+    write-verbose "Testing $path"
+    if ('5B89F086B5124CE77D832F933AC9D1340E025DD2088EC0ECB47275F0F2458AA2' -ne (Get-FolderHash -Path $path)){
+        Write-Warning "Script differences found in: $path"
+        $ret = $false
+    }
+
+    $path = "$InstallMediaPath\PSModules\dbatools"
+    write-verbose "Testing $path"
+    if ('4AA4FA71CD4F62EFFE0E9F28E5709373FD26430C380FEE2A977F30FA88034AAE' -ne (Get-FolderHash -Path $path)){
+        Write-Warning "Script differences found in: $path"
+        $ret = $false
+    }
+
+    $path = "$InstallMediaPath\PSModules\NetworkingDsc"
+    write-verbose "Testing $path"
+    if ('7992DBEAC119FDDF3498CD66C5AAE9A9B07C6B9629941D6E459C2F6A4B243FC4' -ne (Get-FolderHash -Path $path)){
+        Write-Warning "Script differences found in: $path"
+        $ret = $false
+    }
+
     $path = "$InstallMediaPath\PSModules\PendingReboot"
     write-verbose "Testing $path"
     if ('40E0AAD21BE1ECDE7888F2DA2D2404CCF97FC0BA5490FC5060804BF0E2D544F2' -ne (Get-FolderHash -Path $path)){
+        Write-Warning "Script differences found in: $path"
+        $ret = $false
+    }
+
+#TODO:  resolve which version of PSFramework is needed and add in correct test
+
+    $path = "$InstallMediaPath\PSModules\SqlServer"
+    write-verbose "Testing $path"
+    if ('8EB09A86E5F8B4573CB5346BCCE21DEA5DC7F6202546B76D51477C3E4E56B6C1' -ne (Get-FolderHash -Path $path)){
+        Write-Warning "Script differences found in: $path"
+        $ret = $false
+    }
+
+    $path = "$InstallMediaPath\PSModules\SqlServerDsc"
+    write-verbose "Testing $path"
+    if ('1B61857D2DEC26F9FC5C2122967987A27883B6412D40C10FF62F68F986CB9BE1' -ne (Get-FolderHash -Path $path)){
+        Write-Warning "Script differences found in: $path"
+        $ret = $false
+    }
+
+    $path = "$InstallMediaPath\PSModules\StorageDsc"
+    write-verbose "Testing $path"
+    if ('3609B9160F080669A9150CA1225DD332083F986D83CA2CCC7AEB2C543CA4BB8D' -ne (Get-FolderHash -Path $path)){
+        Write-Warning "Script differences found in: $path"
+        $ret = $false
+    }
+
+    $path = "$InstallMediaPath\PSModules\xFailOverCluster"
+    write-verbose "Testing $path"
+    if ('77D45714265F4DF6CD5A3BDF9FBEDEFE7ECCDE61CA7C9C37677F88E5927DD9EC' -ne (Get-FolderHash -Path $path)){
         Write-Warning "Script differences found in: $path"
         $ret = $false
     }
